fe32241f6e34b8bed1005f7a502c42c2b2d4b9c3
Alexandre Pauwels <alex@pauwelslabs.com>
2023-06-06 02:50:17 +0100

    fix: adds instructions for running rename script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # template-nodejs-express
+
+# Init
+
+Run `./template.sh` to rename `package.json` and `README.md` and you
+should be good to go!
--- template.sh
+++ template.sh
@@ -1,6 +1,9 @@
 OLD_NAME="template-nodejs-express"
 SERVICE_DIR=$(git rev-parse --show-toplevel)
 NEW_NAME=$(basename "$SERVICE_DIR")
+echo "Renaming all instances of \"$OLD_NAME\" to \"$NEW_NAME\""
 rm -rf node_modules
 rm -rf package-lock.json
-grep -rl "$OLD_NAME" . --exclude-dir=.git --exclude=template.sh | xargs sed -i 's/$OLD_NAME/$NEW_NAME/g'
+grep -rl "$OLD_NAME" . --exclude-dir=.git --exclude=template.sh | xargs sed -i "s/$OLD_NAME/$NEW_NAME/g"
+npm install
+echo "Rename complete, this script will now delete itself"
