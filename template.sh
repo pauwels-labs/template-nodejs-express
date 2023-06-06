a9514f45f6020716b3345c9d04b404aa8fb3c7f9
Alexandre Pauwels <alex@pauwelslabs.com>
2023-06-06 14:34:07 +0100

    fix: moves script removal to before git operations

--- template.sh
+++ template.sh
@@ -6,11 +6,11 @@ rm -rf node_modules
 rm -rf package-lock.json
 grep -rl "$OLD_NAME" . --exclude-dir=.git --exclude=template.sh | xargs sed -i "s/$OLD_NAME/$NEW_NAME/g"
 npm install
+echo "Rename complete, this script will now delete itself"
+rm -rf template.sh
 echo "Committing and pushing changes"
 git add .
 git commit -am "fix: renames files via template.sh script"
 git push
 git tag v0.0.1
 git push --tags
-echo "Rename complete, this script will now delete itself"
-rm -rf template.sh
